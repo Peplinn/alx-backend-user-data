c3353ce6f01378a6603b621a36cabc377bbd9459
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-11-13 23:17:14 +0100

    Request Validation; Error Checking; PEP8 Unchecked

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Authentication Class
+"""
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+
+        checked_path = path.rstrip('/')
+        checked_excl_path = [p.rstrip('/') for p in excluded_paths]
+
+        if checked_path not in checked_excl_path:
+            return True
+        else:
+            return False
+
+    def authorization_header(self, request=None) -> str:
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
