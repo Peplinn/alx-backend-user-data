15a31849cc28147a79985c645b921da0867b2d26
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-11-14 08:29:25 +0100

    Added File Documentation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,8 +6,18 @@ from typing import List, TypeVar
 
 
 class Auth:
-
+    """_summary_
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """_summary_
+
+        Args:
+                path (str): _description_
+                excluded_paths (List[str]): _description_
+
+        Returns:
+                bool: _description_
+        """
         if path is None:
             return True
         elif excluded_paths is None or excluded_paths == []:
@@ -22,9 +32,17 @@ class Auth:
             return False
 
     def authorization_header(self, request=None) -> str:
+        """_summary_
+
+        Args:
+                request (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+                str: _description_
+        """
         if request is None:
             return None
-        
+
         header = request.headers.get('Authorization')
 
         if header is None:
